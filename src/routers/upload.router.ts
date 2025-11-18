653723fc57409b83485b58b04e91de86bef375bb
TufanRai <thulungtufan16@gmail.com>
2025-11-18 13:41:42 +0545

    Append: added get specific and all files controller

--- src/routers/upload.router.ts
+++ src/routers/upload.router.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 import upload from "../middleware/upload.middleware";
 import {
   deleteFile,
+  getAllFiles,
+  getSpecificFile,
   updateFile,
   uploadFile,
 } from "../controller/uploads.controller";
@@ -10,18 +12,23 @@ import { Roles } from "../interface/Role.enum";
 
 const uploadRouter = Router();
 
+uploadRouter.get("/", getAllFiles);
+uploadRouter.get("/:id", getSpecificFile);
+
 uploadRouter.post(
   "/upload",
   authAdmin([Roles.admin]),
   upload.single("file"),
   uploadFile
 );
+
 uploadRouter.put(
   "/upload/:id",
   authAdmin([Roles.admin]),
   upload.single("file"),
   updateFile
 );
+
 uploadRouter.delete("/upload/:id", authAdmin([Roles.admin]), deleteFile);
 
 export default uploadRouter;
